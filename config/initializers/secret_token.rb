e917fb4ee64749aae97fa5f7ec5bc17416879ee9
vadimvasilenko <vadimtalne@gmail.com>
2011-08-20 20:53:39 +0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Condor::Application.config.secret_token = '11e5e05a274539c3e4343f3374e4dcc7b29866dc9a85da707729656f732b2e163b75ffa16f3b5e24f4e43c66471ea36607d07f9f91efb257acf8817a59a11316'
---
+++ test/test_helper.rb
@@ -0,0 +1,16 @@
+ENV["RAILS_ENV"] = "test"
+require File.expand_path('../../config/environment', __FILE__)
+require 'rails/test_help'
+
+class ActiveSupport::TestCase
+  def login_as(user)
+    session[:user_id] = users(user).id
+  end
+  def logout
+    session.delete :user_id
+  end
+  def setup
+    login_as :one if defined? session
+  end
+
+end
